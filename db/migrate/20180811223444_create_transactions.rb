c68c48f223889a20ed54bb1a933513cd45d1cccc
Winston Galas <BrianisWinston@yahoo.com>
2018-08-11 15:42:30 -0700

    Create types in transaction migration

--- db/migrate/20180811223444_create_transactions.rb
+++ db/migrate/20180811223444_create_transactions.rb
@@ -1,7 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[5.2]
   def change
     create_table :transactions do |t|
-
+      t.string :sender, null: false
+      t.string :receiver, null: false
+      t.float :amount, null: false
       t.timestamps
     end
   end
