fc80a0995e1c9215de655337f3d3a21b27176edb
Winston Galas <BrianisWinston@yahoo.com>
2018-08-11 15:50:05 -0700

    Add annotations and model level validations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id         :bigint(8)        not null, primary key
+#  sender     :string           not null
+#  receiver   :string           not null
+#  amount     :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Transaction < ApplicationRecord
+    validates :sender, :receiver, :amount, presence: true
 end
--- test/models/transaction_test.rb
+++ test/models/transaction_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id         :bigint(8)        not null, primary key
+#  sender     :string           not null
+#  receiver   :string           not null
+#  amount     :float            not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'test_helper'
 
 class TransactionTest < ActiveSupport::TestCase
