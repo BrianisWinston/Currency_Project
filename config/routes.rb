f37a58ff5f41d64c80e0cf42ec5f233930e96dce
Winston Galas <BrianisWinston@yahoo.com>
2018-08-14 14:21:01 -0700

    Create comment logic for views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  # Set default page to users index
   root 'api/v1/users#index'
+
+  # Created nested routes by way of namespacing
   namespace :api do
     namespace :v1 do
+      #Create routes only for certain methods
       resources :users, only: [:create, :index, :new]
       resource :session, only: [:create, :new, :destroy]
       resources :transactions, only: [:create, :index]
+      #Create JSON route in order for JBuilder to render JSON
       defaults format: :json do
         get 'users/current', :to => 'users#current'
       end
